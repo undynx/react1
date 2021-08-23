61fbbabff79bb5ee7a75f58f3d145a2441d6b9cf
undynx <martinezf.maite@gmail.com>
2021-08-22 22:32:45 -0300

    agrega navbar y bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <h1 className="display-2" style={{ textAlign: 'center' }}>ReactJS</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,36 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+
+function NavBar() {
+    return (
+        <Navbar className="bg-light">
+            <Container>
+                <Navbar.Brand href="#home">Movida Roller</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Freeskate" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="https://www.rollerblade.com/uruguay/skates/?/all/freeskate">Rollerblade</NavDropdown.Item>
+                            <NavDropdown.Item href="http://www.sebaskates.com/en/skates/?pg=3">SEBA - FR</NavDropdown.Item>
+                            <NavDropdown.Item href="https://flyingeagleskate.com/collections/adult-inline-skates">Flying Eagle</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Velocidad" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="https://www.rollerblade.com/uruguay/skates/speed/">Rollerblade</NavDropdown.Item>
+                            <NavDropdown.Item href="http://www.sebaskates.com/en/skates/?pg=2">SEBA - FR</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Niñes" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="https://www.rollerblade.com/uruguay/skates/?/boys">Rollerblade</NavDropdown.Item>
+                            <NavDropdown.Item href="http://www.sebaskates.com/en/skates/?pg=6">SEBA - FR</NavDropdown.Item>
+
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar >
+    )
+}
+
+export default NavBar;
